2d1811177609060f62b2ae8eaaec4345927b220c
Legaz-le <188694281+Legaz-le@users.noreply.github.com>
2026-04-07 15:44:50 +0900

    db fixed issue with neo

--- db/index.ts
+++ db/index.ts
@@ -1,5 +1,3 @@
 import { drizzle } from 'drizzle-orm/neon-serverless'
-import { neon } from '@neondatabase/serverless'
 
-const sql = neon(process.env.DATABASE_URL!)
-export const db = drizzle(sql)
+export const db = drizzle(process.env.DATABASE_URL!)
